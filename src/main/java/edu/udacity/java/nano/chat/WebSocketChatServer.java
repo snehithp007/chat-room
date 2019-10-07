61c3d4cf685f0531cd435cf725e2ed2cf1a966eb
Snehith <snehithp@gmail.com>
2019-10-06 22:40:44 -0700

    Initial commit

---
+++ src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
@@ -0,0 +1,86 @@
+package edu.udacity.java.nano.chat;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * WebSocket Server
+ *
+ * @see ServerEndpoint WebSocket Client
+ * @see Session   WebSocket Session
+ */
+
+@Component
+@ServerEndpoint("/chat/{username}")
+public class WebSocketChatServer {
+
+    /**
+     * All chat sessions.
+     */
+    private static Map<String, Session> onlineSessions = new ConcurrentHashMap<>();
+
+    private static void sendMessageToAll(String msg) {
+        onlineSessions.forEach((k,v) -> {
+            try {
+                v.getBasicRemote().sendText(msg);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    /**
+     * Open connection, 1) add session, 2) add user.
+     */
+    @OnOpen
+    public void onOpen(Session session, @PathParam("username") String username) {
+        onlineSessions.put(username, session);
+        Message message = new Message();
+        message.setUsername(username);
+        message.setMsg( username + " has entered chat room!!");
+        message.setType("ENTER");
+        message.setOnlineCount(onlineSessions.size());
+        sendMessageToAll(JSON.toJSONString(message));
+    }
+
+    /**
+     * Send message, 1) get username and session, 2) send message to all.
+     */
+    @OnMessage
+    public void onMessage(Session session, String jsonStr) {
+        JSONObject jsonObject = JSON.parseObject(jsonStr);
+        Message message = JSON.toJavaObject(jsonObject, Message.class);
+        message.setType("SPEAK");
+        message.setOnlineCount(onlineSessions.size());
+        sendMessageToAll(JSON.toJSONString(message));
+    }
+
+    /**
+     * Close connection, 1) remove session, 2) update user.
+     */
+    @OnClose
+    public void onClose(Session session) {
+        onlineSessions.values().removeIf(value -> value.getId().equals(session.getId()));
+        Message message = new Message();
+        message.setOnlineCount(onlineSessions.size());
+        message.setType("LEAVE");
+        sendMessageToAll(JSON.toJSONString(message));
+    }
+
+    /**
+     * Print exception.
+     */
+    @OnError
+    public void onError(Session session, Throwable error) {
+        error.printStackTrace();
+    }
+
+}
